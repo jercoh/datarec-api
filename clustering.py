8f7a16ee1d9314c7b1f231c17eb8baceff83b0a4
jercoh <jejechaber289@msn.com>
2014-04-11 14:42:08 +0200

    k-means

--- clustering.py
+++ clustering.py
@@ -1,6 +1,8 @@
 from math import pow
+import numpy
+import random
 
-def distance(user1,user2):
+def distance_json(user1,user2):
 	if len(user1.items()) != len(user2.items()):
 		return False
 	else:
@@ -12,4 +14,27 @@ def distance(user1,user2):
 			else:
 				return False
 		return distance
+
+def distance(u,v):
+	return numpy.linalg.norm(u-v)
+
+
+def k_means(training_set, k, number_of_iter):
+	m = training_set.shape[0]
+	centroids = random.sample(training_set, k)
+	for i in range(0:number_of_iter):
+		idx = [0]*m
+		for j in range(0:m):
+			current = training_set[j]
+			distance_to_centroids_vector = ((centroids-current).dot((centroids-current).T)).diagonal()
+			idx[j] = distance_to_centroids_vector.argmin()
+		for l in range(0:k):
+			cluster_size = 0
+			new_centroid = numpy.zeros(training_set.shape[1])
+			for t in range(0:m):
+				if idx[t] == l:
+					cluster_size += 1
+					new_centroid += training_set[t]
+			centroids[l] = new_centroid/cluster_size
+
 
