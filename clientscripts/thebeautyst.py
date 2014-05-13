888a43cb677f81d255309a019db0ae2b73850e2c
jercoh <jejechaber289@msn.com>
2014-05-13 18:53:21 +0200

    'template'

--- clientscripts/thebeautyst.py
+++ clientscripts/thebeautyst.py
@@ -4,6 +4,9 @@ from helper.importer import Importer
 from datarec.recommender import ItemRecommender
 import datarec.hot_ranking as ranking
 from datetime import datetime
+from mako.template import Template
+from mako.lookup import TemplateLookup
+import mandrill
 
 dbName = 'test_database'
 baserUrl = 'thebeautyst.org'
@@ -11,6 +14,9 @@ dataToImport = ['products', 'posts?type=photos', 'posts?type=videos', 'posts?typ
 db = pymongo.MongoClient()[app_config.Config(dbName).MONGODB_DB]
 collectionNames = ['products', 'posts']
 contentTypes = ['likes_photos', 'likes_videos', 'likes_post', 'purchases']
+templatePath = ''
+mylookup = TemplateLookup(directories=[''],input_encoding='utf-8',output_encoding='utf-8',)
+mandrill_client = mandrill.Mandrill('QVQhU-pdRHiaPGWceEKZfg')
 
 
 def importData():
@@ -42,3 +48,58 @@ def calculate_popularity():
 			else:
 				score = ranking.hot(item["likes"], datetime.strptime(item["created_at"], '%Y-%m-%d %H:%M:%S'))
 			collection.update({"id": item["id"]}, {"$set": {"score": score}}, True)
+
+def send_newsletter():
+	mytemplate = mylookup.get_template(template)
+	collection_users = db['users']
+	collection_posts = db['posts']
+	collection_products = db['products']
+	#users = list(collection_users.find())
+	# for user in users:
+	user = list(collection_users.find({'id':'49'}))[0]
+	try:
+		recommended_video = list(collection_posts.find({'id': user['recommendation_likes_videos'][0], 'type': 'videos'}))[0]
+	except:
+		recommended_video = list(collection_posts.find({'type':'videos'}).sort([('score',-1)]).limit(-1))[0]
+	try:
+		recommended_article = list(collection_posts.find({'id': user['recommendation_likes_post'][0], 'type': 'post'}))[0]
+	except:
+		recommended_article = list(collection_posts.find({'type':'post'}).sort([('score',-1)]).limit(-1))[0]
+	try:
+		product_ids = user['recommendation_purchases']
+	except:
+		product_ids = []
+	recommended_products = []
+	if (product_ids != []):
+		for product_id in product_ids:
+			recommended_products.append(list(collection_products.find({'id':product_id}))[0])
+	else:
+		recommended_products = list(collection_products.find().sort([('score',-1)]).limit(-8))
+	html = mytemplate.render(video=recommended_video, article= recommended_article, products = recommended_products)
+	mandrill_client.messages.send(message=message(html, 'jeremcoh@gmail', user['first_name']+user['last_name']), async=False, ip_pool='Main Pool', send_at='')
+
+def message(html, email, name):
+	message = {
+		'attachments': None,
+        'auto_html': None,
+        'auto_text': None,
+        'from_email': 'jeremie@datarec.io',
+		 'from_name': 'Jeremie Cohen',
+		 'headers': {'Reply-To': 'jeremie@datarec.io'},
+		 'html': html,
+		 'important': False,
+		 'metadata': {'website': 'www.datarec.io'},
+		 'preserve_recipients': None,
+		 'recipient_metadata': [{'rcpt': email,
+		                         'values': {'user_id': 123456}}],
+		 'subject': 'Test Email',
+		 'tags': ['thebeautyst'],
+		 'to': [{'email': email,
+		         'name': name,
+		         'type': 'to'}],
+		 'track_clicks': True,
+		 'track_opens': True,
+		 'tracking_domain': True
+	}
+	return message
+
